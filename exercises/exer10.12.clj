7e7eb41deea40a6b2c86e7653ae221b8929f9bef
Mark Simpson <verdammelt@gmail.com>
2013-03-12 21:12:31 -0400

    Improving my answer after seeing solution.

--- exercises/exer10.12.clj
+++ exercises/exer10.12.clj
@@ -9,11 +9,10 @@
 ;; less than 100. Duplicates are OK.  
 ;; Hint: You’ll need two steps and a body that just returns a value.
 (def all-non-primes
-  (fn []
-    (with-monad sequence-m 
-      (domonad [n (range 2 100)
-                m (multiples n)]
-               m))))
+  (with-monad sequence-m 
+    (domonad [n (range 2 11)          ; only need to go up to SQRT(100)
+              m (multiples n)]
+             m)))
 
 ;; Use sets to calculate all the primes less than 100.
 ;; Hint: Here’s how you can tell if 6 is a non-prime less than 10:
@@ -21,7 +20,8 @@
 ;; 6
 ;; Hint: You’ll likely want to use remove.
 (def primes-less-than-100
-  (fn []
-    (let [possibles (set (range 2 100))
-          all-non (set (all-non-primes))]
-      (remove (fn [n] (all-non n)) possibles))))
+  (remove (set all-non-primes) (range 2 101)))
+
+
+
+
