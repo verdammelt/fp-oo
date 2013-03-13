231d607f67bbdc1e3dee6824b701d2d170b03f74
Mark Simpson <verdammelt@gmail.com>
2013-03-12 21:08:50 -0400

    Doing exercises in section exer 10.12
    Not sure if the last one is the answer intended - will check solutions.

---
+++ exercises/exer10.12.clj
@@ -0,0 +1,27 @@
+(use 'clojure.algo.monads)
+
+;; write multiples which return all non-prime multiples of a number n less than 100
+(def multiples 
+  (fn [n]
+    (range (* 2 n) 101 n)))
+
+;; Use the Sequence monad or for (your choice!) to find all non-primes
+;; less than 100. Duplicates are OK.  
+;; Hint: You’ll need two steps and a body that just returns a value.
+(def all-non-primes
+  (fn []
+    (with-monad sequence-m 
+      (domonad [n (range 2 100)
+                m (multiples n)]
+               m))))
+
+;; Use sets to calculate all the primes less than 100.
+;; Hint: Here’s how you can tell if 6 is a non-prime less than 10:
+;; user=> (#{4 6 8 9} 6)
+;; 6
+;; Hint: You’ll likely want to use remove.
+(def primes-less-than-100
+  (fn []
+    (let [possibles (set (range 2 100))
+          all-non (set (all-non-primes))]
+      (remove (fn [n] (all-non n)) possibles))))
