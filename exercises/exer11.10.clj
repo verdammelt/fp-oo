b718ffb80d7a3e9b80e8262fa72dd5d6dd8a210e
Mark Simpson <verdammelt@gmail.com>
2013-05-06 10:13:44 -0400

    Answers for exercise 1

--- exercises/exer11.10.clj
+++ exercises/exer11.10.clj
@@ -1,3 +1,12 @@
 ;; building a zipper!
 
+(def zseq
+  (fn [tree] tree))
+
+(def zdown (fn [zip] (first zip)))
+(def znode (fn [zip] zip))
+
+(assert (= (-> '(a b c) zseq zdown znode) 'a))
+(assert (= (-> '((+ 1 2) 3 4) zseq zdown znode) '(+ 1 2)))
+(assert (= (-> '((+ 1 2) 3 4) zseq zdown zdown znode) '+))
 
