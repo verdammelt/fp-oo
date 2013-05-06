8bcf6a9ba23c43e6f088595cbb897b51ad1046ef
Mark Simpson <verdammelt@gmail.com>
2013-05-06 10:39:46 -0400

    Exercise 2 done.
    got a bit confused but worked through it.

--- exercises/exer11.10.clj
+++ exercises/exer11.10.clj
@@ -1,12 +1,35 @@
 ;; building a zipper!
 
 (def zseq
-  (fn [tree] tree))
+  (fn [tree]
+    {:here tree
+     :parents '()}))
 
-(def zdown (fn [zip] (first zip)))
-(def znode (fn [zip] zip))
+(def zroot 
+  (fn [zip] 
+    (let [parents (:parents zip)]
+      (if (empty? parents) (znode zip)
+          (znode (last (:parents zip)))))))
 
+(def znode 
+  (fn [zip] (:here zip)))
+
+(def zdown 
+  (fn [zip] 
+    (let [here (first (:here zip))]
+      (and here 
+           (merge zip {:here here :parents (conj (:parents zip) zip)})))))
+
+(def zup 
+  (fn [zip] (first (:parents zip))))
+
+(assert (= (-> '(a b c) zseq znode) '(a b c)))
 (assert (= (-> '(a b c) zseq zdown znode) 'a))
+(assert (= (-> '(a b c) zseq zup) nil))
+(assert (= (-> '() zseq zdown) nil))
+(assert (= (-> '(a b c) zseq zdown zup znode) '(a b c)))
 (assert (= (-> '((+ 1 2) 3 4) zseq zdown znode) '(+ 1 2)))
 (assert (= (-> '((+ 1 2) 3 4) zseq zdown zdown znode) '+))
+(assert (= (-> '(a) zseq zroot) '(a)))
+(assert (= (-> '(((a)) b c) zseq zdown zdown zdown zroot) '(((a)) b c))) 
 
